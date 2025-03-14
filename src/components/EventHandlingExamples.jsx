8e9be9e67369ba75b21d847ac7d4a35149e6f255
Annibal Yazbek <annibal.yazbek@fhsoftware.com.br>
2025-03-14 16:06:25 -0300

    Atualizando EventHandlingExamples

--- src/components/EventHandlingExamples.jsx
+++ src/components/EventHandlingExamples.jsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
 
 const EventHandlingExamples = () => {
+ 
+  const handleClick = () => {
+    alert ("Testando");
+  };
+
+  const handleGreetings = (name) => {
+    alert(`Olá ${name}`);
+  };
+
+  const [name,setName] = useState("");
+
+  // Evento de formulário - evitando recarga da pagina
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert(`enviado!" ${name}`);
+  };
+
   return (
     <div>
       <button onClick={() => alert("oi")}>Clique aqui</button>
+      <button onClick={handleClick}>Clique aqui 2</button>
+      <br />
+      <button onClick={() => handleGreetings("Ana")}>Dizer Olá Ana</button>
+      <button onClick={() => handleGreetings("Pedro")}>Dizer Olá Pedro</button>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Digite seu nome"/>
+        <input type="submit" value="Enviar" />
+      </form>
+
     </div>
   );
 };
